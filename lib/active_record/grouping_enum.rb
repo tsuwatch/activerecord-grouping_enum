614461b9acab1ede1d5c0e358266b4c4d7d71ef6
Tomohiro Suwa <neoen.gsn@gmail.com>
2015-12-28 20:18:51 +0900

    Implement

--- lib/active_record/grouping_enum.rb
+++ lib/active_record/grouping_enum.rb
@@ -1,6 +1,42 @@
 module ActiveRecord
   module GroupingEnum
-    def grouping_enum
+    def grouping_enum(definitions)
+      definitions.each do |name, values|
+        name = name.to_sym
+        klass = self
+        values.each do |key, enums|
+          _grouping_enum_methods_module.module_eval do
+            klass.send(:detect_enum_conflict!, name, "#{key}?")
+            define_method("#{key}?") do
+              values = []
+              enums.each do |enum|
+                values << klass.defined_enums[name.to_s].fetch(enum.to_s)
+              end
+              values.include?(self[name])
+            end
+
+            klass.send(:detect_enum_conflict!, name, "#{key}", true)
+            klass.scope "#{key}", lambda {
+              query = "#{name} == #{klass.defined_enums[name.to_s][enums.shift.to_s]}"
+              enums.each do |enum|
+                query << " or #{name} == #{klass.defined_enums[name.to_s][enum.to_s]}"
+              end
+              klass.where(query)
+            }
+          end
+        end
+      end
+    end
+
+    private
+
+    def _grouping_enum_methods_module
+      @_grouping_enum_methods_module ||=
+        begin
+          mod = Module.new
+          include mod
+          mod
+        end
     end
   end
 end
--- spec/active_record/grouping_enum_spec.rb
+++ spec/active_record/grouping_enum_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe ActiveRecord::GroupingEnum do
+  class Post < ActiveRecord::Base
+    enum status: { editing: 0, closed: 1, published: 2, deleted: 3, admin_deleted: 4 }
+    grouping_enum status: {
+      undeleted: [:editing, :closed, :published]
+    }
+  end
+
   it { expect(Post).to respond_to(:grouping_enum) }
+  it { expect { Post.grouping_enum status: { deleted: [:deleted] } }.to raise_error ArgumentError }
+
+  describe '.undeleted' do
+    subject { Post }
+
+    let!(:editing_post) { Post.create(status: :editing) }
+    let!(:closed_post) { Post.create(status: :closed) }
+    let!(:published_post) { Post.create(status: :published) }
+    let!(:deleted_post) { Post.create(status: :deleted) }
+
+    it { expect(subject.undeleted.to_a).to eq [editing_post, closed_post, published_post] }
+  end
+
+  describe '#undeleted?' do
+    subject { Post.create(attr) }
+    let(:attr) { { status: :editing } }
+
+    it { is_expected.to respond_to(:undeleted?) }
+
+    context 'status is closed' do
+      let(:attr) { { status: :closed } }
+
+      it { expect(subject.undeleted?).to eq true }
+    end
+
+    context 'status is published' do
+      let(:attr) { { status: :published } }
+
+      it { expect(subject.undeleted?).to eq true }
+    end
+
+    context 'status is deleted' do
+      let(:attr) { { status: :deleted } }
+
+      it { expect(subject.undeleted?).to eq false }
+    end
+  end
 end
